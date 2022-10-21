5b1c2cba8890cb8d6a415c7386ea160a64489b23
Abenina14 <abenynin2314@gmail.com>
2022-10-21 12:35:37 +0300

    grading student

---
+++ Grading Student(HackerRank).py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    lessT = []
+    for i in grades:
+        if (i > 37 and (((((i // 5)+1)*5) - i) < 3)):
+            lessT.append(((i//5)+1)*5)
+        else:
+            lessT.append(i)
+    return lessT
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
