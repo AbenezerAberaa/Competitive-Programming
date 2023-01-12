810bb90d7bf8e5100ba4281f50741cc8f50f1e56
Abenina14 <abenynin2314@gmail.com>
2023-01-12 09:07:01 +0300

    Sort Colors

---
+++ Sort colors(LeetCode).py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        zero=nums.count(0)
+        one=nums.count(1)
+        two=nums.count(2)
+        for x in range(len(nums)):
+            if two:
+                nums.pop()
+                nums.insert(0,2)
+                two-=1
+            elif one:
+                nums.pop()
+                nums.insert(0,1)
+                one-=1
+            else:
+                nums.pop()
+                nums.insert(0,0)
+
