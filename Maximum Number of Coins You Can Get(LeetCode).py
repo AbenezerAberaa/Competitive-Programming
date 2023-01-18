9457b014cdedfd9a374d16bdbaf91a904588b71c
Edilcc <abenynin2314@gmail.com>
2023-01-18 23:40:45 +0300

    max no of coins u can get

---
+++ Maximum Number of Coins You Can Get(LeetCode).py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxCoins(self, piles: List[int]) -> int:
+        sortedPiles = sorted(piles, reverse=True)
+        result = 0
+        cnt = len(piles) / 3
+
+        for i in range(len(piles)) :
+            if not cnt :
+                return result
+            if i % 2 == 1 : 
+                result += sortedPiles[i]
+                cnt -= 1
