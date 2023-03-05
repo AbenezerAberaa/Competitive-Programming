89dab828ef1329e512c1420ade67d7d638a3e537
Edilcc <abenynin2314@gmail.com>
2023-03-05 16:12:28 +0300

    Pascal triangle 2

---
+++ Pascal's Triangle II(LeetCode).py
@@ -0,0 +1,11 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+
+        result = [1]
+
+        for i in range(1,rowIndex+1):
+            result.append(1)
+            for j in range(len(result)-2, 0 , -1):
+                result[j] += result[j-1]
+
+        return result
