4755daa9a041f703d857ebaf2672b24aca194c98
Abenezer Abera <abenynin2314@gmail.com>
2023-04-11 08:34:19 -0700

    Create Maximum Matching of Players With Trainers

---
+++ Maximum Matching of Players With Trainers(LeetCode).py
@@ -0,0 +1,15 @@
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+
+        left = 0
+        right = 0
+        count = 0
+        players.sort()
+        trainers.sort()
+        while left < len(players) and right < len(trainers):
+            while left < len(players) and right < len(trainers) and players[left] <= trainers[right]:
+                count += 1
+                left += 1
+                right += 1
+            right += 1
+        return count
