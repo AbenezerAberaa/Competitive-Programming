bb0ead8f920c6b3b76155974f4ac48497db57009
Abenezer Abera <abenezer.aberaa@gmail.com>
2023-10-18 10:31:50 +0300

    Count Primes

---
+++ Count Primes(LeetCode).py
@@ -0,0 +1,24 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+
+        def count(n : int):
+            primes = [True] * n
+            primes[0] = primes[1] = False
+            i = 0
+            
+            while i < ceil(sqrt(n)):
+                if primes[i]: 
+                    j = i * i
+                    
+                    while j < n:
+                        primes[j] = False
+                        j += i
+                
+                i += 1
+            
+            return primes
+        
+        if n <= 2:
+            return 0
+        
+        return count(n).count(True)
