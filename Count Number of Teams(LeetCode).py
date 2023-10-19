19134aac0c56b9986c0bc38859cef1f089c741af
Abenezer Abera <abenezer.aberaa@gmail.com>
2023-10-20 00:34:32 +0300

    Count Number of Teams

---
+++ Count Number of Teams(LeetCode).py
@@ -0,0 +1,14 @@
+class Solution:
+    def numTeams(self, rating: List[int]) -> int:
+
+        ans = 0
+        seen = [[0]*2 for _ in rating]
+        for i in range(len(rating)): 
+            for ii in range(i): 
+                if rating[ii] < rating[i]: 
+                    ans += seen[ii][0]
+                    seen[i][0] += 1
+                elif rating[ii] > rating[i]: 
+                    ans += seen[ii][1]
+                    seen[i][1] += 1
+        return ans 
